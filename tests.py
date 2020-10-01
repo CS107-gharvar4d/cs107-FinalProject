393e0a57a84015cf5b815cb4ddc601796258ec4c
boerz-coding <boerzhang@g.harvard.edu>
2020-10-01 19:54:36 -0400

    modifying travis and test

--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
 #adding nothing but testing push to TRAVISCI
 import unittest
 
-import awesome
+#import awesome
 
-
+'''
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
-
+'''
 
 if __name__ == '__main__':
     unittest.main()
