1ef68329276317679a64c066954aed09d60c9daf
dweiharvard <43420298+dweiharvard@users.noreply.github.com>
2020-11-17 20:08:11 -0500

    Partial Deriv Feature

--- code/jacobian.py
+++ code/jacobian.py
@@ -14,7 +14,7 @@ class AutoDiffVector():
         return AutoDiffVector(np.array([ii.val for ii in v]),np.array([ii.der for ii in v]))
 
     def __add__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val+=other.val
             new.der+=other.der
@@ -26,7 +26,7 @@ class AutoDiffVector():
         return self.__add__(other)
 
     def __mul__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val*=other.val
             new.der=self.der*other.val+self.val*other.der
@@ -39,7 +39,7 @@ class AutoDiffVector():
         return self.__mul__(other)
 
     def __truediv__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val=self.val/other.val
             new.der=self.der/other.val-self.val/np.power(other.val,2.)*other.der
@@ -49,7 +49,7 @@ class AutoDiffVector():
         return new
 
     def __truerdiv__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val=other.val/self.val
             new.der=other.der/self.val-other.val/np.power(self.val,2.)*self.der
@@ -59,13 +59,13 @@ class AutoDiffVector():
         return new
 
     def __neg__(self):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         new.val=-new.val
         new.der=-new.der
         return new
 
     def __sub__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val-=other.val
             new.der-=other.der
@@ -77,7 +77,7 @@ class AutoDiffVector():
         return -self.__sub__(other)
 
     def __pow__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val=np.power(self.val,other.val)
             new.der=other.val*np.power(self.val,other.val-1)*self.der+new.val*np.log(self.val)*other.der
@@ -87,7 +87,7 @@ class AutoDiffVector():
         return new
 
     def __rpow__(self,other):
-        new=copy.copy(self)
+        new=copy.deepcopy(self)
         try:
             new.val=np.power(other.val,self.val)
             new.der=self.val*np.power(other.val,self.val-1)*other.der+new.val*np.log(other.val)*self.der
@@ -96,26 +96,39 @@ class AutoDiffVector():
             new.der=new.val*np.log(other)*self.der
         return new
 
+    def partial(self,vari):
+        try:
+            idx=np.nonzero(vari.der)[0]
+            if len(idx)>1:
+               print('Not an independent variable')
+               raise TypeError
+            if len(f.der.shape)==1:
+               f.der=f.der.reshape(1,-1)
+            return f.der[:,idx[0]]
+        except AttributeError:
+            print('Not an independent variable')
+            raise TypeError
+
 def sin_ad(x):
-    y=copy.copy(x)
+    y=copy.deepcopy(x)
     y.val=np.sin(x.val)
     y.der=np.cos(x.val)*x.der
     return y
 
 def cos_ad(x):
-    y=copy.copy(x)
+    y=copy.deepcopy(x)
     y.val=np.cos(x.val)
     y.der=-np.sin(x.val)*x.der
     return y
 
 def tan_ad(x):
-    y=copy.copy(x)
+    y=copy.deepcopy(x)
     y.val=np.tan(x.val)
     y.der=np.power(np.sec(x.val),2.)*x.der
     return y
 
 def exp_ad(x):
-    y=copy.copy(x)
+    y=copy.deepcopy(x)
     y.val=np.exp(x.val)
     y.der=np.exp(x.val)*x.der
     return y
@@ -134,7 +147,11 @@ def gen_vars(vvars):
 
 [x,y,z,t]=gen_vars([3.,np.pi,5.,3.4])
 f = AutoDiffVector.vconvert([(x + y**z)/t, sin_ad(x+cos_ad(100*y**3)-z**t)])
-print(f.val,f.der)
+print(f.val,f.der,f.partial(t))
+
+[x,y,z,t]=gen_vars([3.,np.pi,5.,3.4])
+f = (x + y**z)/t
+print(f.val,f.der,f.partial(t))
 
 v_list=gen_vars(np.linspace(1,1,10000))
 f = mul_ad(v_list)
