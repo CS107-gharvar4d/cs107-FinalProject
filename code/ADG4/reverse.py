939b6bd2db3dc13163e6b270bc2ea7b69f870537
dweiharvard <43420298+dweiharvard@users.noreply.github.com>
2020-12-06 17:40:33 -0500

    12/06/2020

--- code/ADG4/reverse.py
+++ code/ADG4/reverse.py
@@ -17,28 +17,6 @@ class AutoDiffReverse():
             return self.derivs[wrt]
         return self.derivs
 
-#    def merge_derivs(self, d1, d2, fn=lambda deriv_1, deriv_2: deriv_1 + deriv_2, default_value=0):
-#        res = {}
-#
-#        for var, d1_deriv in d1.items():
-#            if var in d2:
-#                res[var] = fn(d1_deriv,d2[var])
-#            else:
-#                res[var] = fn(d1_deriv,default_value)
-#
-#        for var, d2_deriv in d2.items():
-#            if var not in res:
-#                res[var] = fn(default_value,d2_deriv)
-#        return res
-#
-#
-#
-#    def map_derivs(self, fn=lambda deriv: deriv):
-#        res = self.derivs.copy()
-#        for key, val in res.items():
-#            res[key] = fn(val)
-#        return res
-
 
     def __repr__(self):
         return f'AutoDiffToy({self.val})'
@@ -48,7 +26,6 @@ class AutoDiffReverse():
         
         try:
             new.val+=other.val
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs)
             new.children=[[self,1],[other,1]]
         except AttributeError:
             new.val+=other
@@ -62,24 +39,37 @@ class AutoDiffReverse():
         new=copy.deepcopy(self)
         try:
             new.val*=other.val
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs, lambda new_der, other_der: new_der*other.val + self.val * other_der)
             new.children=[[self,other.val],[other,self.val]]
         except AttributeError:
             new.val*=other
-#            new.derivs = new.map_derivs(lambda deriv: deriv*other)
             new.children=[[self,other]]
         return new
 
     def __rmul__(self,other):
         return self.__mul__(other)
     
-#############################################
     def backprop(self):
+        var_list=[]
+        self._backprop(var_list)
+        self._partial={}
+        for vv in var_list:
+            self._partial[vv]=vv.derivs[vv]
+
+    def _backprop(self,var_list):
         for (c,i) in self.children:
             c.acc = i * self.acc
-            c.backprop()
+            c._backprop(var_list)
         if not self.children:
             self.derivs[self]=self.acc+self.derivs[self]
+            var_list.append(self)
+
+    def partial(self,vv):
+        self.backprop()
+        if vv in self._partial.keys():
+            return self._partial[vv]
+        else:
+            raise KeyError('Function not directly dependent on input')
+            
 
 
 
@@ -88,100 +78,5 @@ x=AutoDiffReverse(3)
 y=AutoDiffReverse(4)
 z=AutoDiffReverse(9)
 m=x+y
-n=m*z
-n.backprop()
-print(n.val,x.get_deriv())
-#
-#
-#
-#
-#
-#
-#    def __radd__(self,other):
-#        return self.__add__(other)
-#
-#    def __mul__(self,other):
-#        new=copy.copy(self)
-#        try:
-#            new.val*=other.val
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs, lambda new_der, other_der: new_der*other.val + self.val * other_der)
-#        except AttributeError:
-#            new.val*=other
-#            new.derivs = new.map_derivs(lambda deriv: deriv*other)
-#        return new
-#
-#    def __rmul__(self,other):
-#        return self.__mul__(other)
-#
-#    def __neg__(self):
-#        new=copy.copy(self)
-#        new.val=-new.val
-#        new.derivs = new.map_derivs(lambda deriv: -deriv)
-#        return new
-#
-#    def __sub__(self,other):
-#        new=copy.copy(self)
-#        try:
-#            new.val-=other.val
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs, lambda new_der,other_der: new_der - other_der)
-#
-#        except AttributeError:
-#            new.val-=other
-#        return new
-#
-#    def __rsub__(self,other):
-#        return -self.__sub__(other)
-#
-#    def __pow__(self,other):
-#        new=copy.copy(self)
-#        try:
-#            new.val=np.power(self.val,other.val)
-#            l = lambda new_der,other_der: other.val*np.power(self.val,other.val-1)*new_der+new.val*np.log(self.val)*other_der
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs, l)
-#        except AttributeError:
-#            new.val=np.power(self.val,other)
-#            new.derivs= new.map_derivs(lambda deriv: other*np.power(self.val,other-1)*deriv)
-#        return new
-#
-#    def __rpow__(self,other):
-#        new=copy.copy(self)
-#        try:
-#            new.val=np.power(other.val,self.val)
-#            l = lambda new_der,other_der: self.val*np.power(other.val,self.val-1)*other_der+new.val*np.log(other.val)*new_der
-#            new.derivs = self.merge_derivs(new.derivs, other.derivs, l)
-#        except AttributeError:
-#            new.val=np.power(other,self.val)
-#            new.derivs= new.map_derivs(lambda deriv: new.val*np.log(other)*deriv)
-#        return new
-#
-## David, 11/08/2020, trying to find a duner method for sin...
-##    def __sin__(self):
-##        new=copy.copy(self)
-##        new.val=np.sin(self.val)
-##        new.der=np.cos(self.val)*self.der
-##        return new
-#
-#def sin_ad(x):
-#    y=copy.copy(x)
-#    y.val=np.sin(x.val)
-#    y.derivs = x.map_derivs(lambda x_der: np.cos(x.val)*x_der)
-#
-#    return y
-#
-#def cos_ad(x):
-#    y=copy.copy(x)
-#    y.val=np.cos(x.val)
-#    y.derivs =  x.map_derivs(lambda x_der: -np.sin(x.val)*x_der)
-#    return y
-#
-#def tan_ad(x):
-#    y=copy.copy(x)
-#    y.val=np.tan(x.val)
-#    y.derivs =  x.map_derivs(lambda x_der: np.power(np.sec(x.val),2.)*x_der)
-#    return y
-#
-#def exp_ad(x):
-#    y=copy.copy(x)
-#    y.val=np.exp(x.val)
-#    y.derivs =  x.map_derivs(lambda x_der: np.exp(x.val)*x_der)
-#    return y
+n=m*z+x
+print(n.val,n.partial(x))
