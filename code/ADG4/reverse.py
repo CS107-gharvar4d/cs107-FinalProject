d57a9f50f9622ebccd6b6e9cd9b7dafa4bf03e82
boerz-coding <62970850+boerz-coding@users.noreply.github.com>
2020-12-07 20:56:04 -0500

    Update reverse.py
    Add sub, div, pow. Changed copy. Add a case of calculating the derivatives of independent variables themselves.

--- code/ADG4/reverse.py
+++ code/ADG4/reverse.py
@@ -1,43 +1,43 @@
 import numpy as np
-from collections import defaultdict
 import copy
+from collections import defaultdict
 
 
 
 class AutoDiffReverse():
     
-    def __init__(self,a):
+    def __init__(self,a, name=None):
         self.val=a
-        self.derivs = { self: 0}
         self.children = []
         self.acc = 1
-
-
-    def get_deriv(self,wrt=None):
-        if wrt:
-            return self.derivs[wrt]
-        return self.derivs
-
+        self.has_backpropped = False
+        self.name = name
+        self._partial = {}
 
     def __repr__(self):
-        return f'AutoDiffToy({self.val})'
+        if not self.name:
+            return f'AutoDiffReverse({self.val})'
+        return f'AutoDiffReverse({self.val}, name="{self.name}")'
 
     def __add__(self,other):
-        new=copy.deepcopy(self)
+        new=AutoDiffReverse(self.val)
+        new.name=None
         
         try:
             new.val+=other.val
             new.children=[[self,1],[other,1]]
         except AttributeError:
             new.val+=other
-            new.children=[(self,1)]
+            new.children=[[self,1]]
         return new
 
     def __radd__(self,other):
         return self.__add__(other)
 
     def __mul__(self,other):
-        new=copy.deepcopy(self)
+        new=AutoDiffReverse(self.val)
+        new.name=None
+        
         try:
             new.val*=other.val
             new.children=[[self,other.val],[other,self.val]]
@@ -50,32 +50,151 @@ class AutoDiffReverse():
         return self.__mul__(other)
     
     def backprop(self):
-        self.acc=1
-        self._partial=defaultdict(int)
-        self._backprop(self._partial)
-
-    def _backprop(self,partial):
-        for (c,i) in self.children:
-            c.acc = i * self.acc
-            c._backprop(partial)
-        if not self.children:
-            partial[self]+=self.acc
+        # A back prop implementation that keeps all derivative accumulations
+        # within this root node that calls .backprop()
+        partial = defaultdict(int)
+        
+        stack = [ (child_node, edge_value) for (child_node, edge_value) in self.children]
+        while stack:
+            # edge value is the derivative between the root node
+            # and this current node
+            node, edge_value = stack.pop()
+            # Update the partial derivative to add this current derivative value
+            partial[node] += edge_value
+            # Add each child to our stack
+            for child_node, child_edge_value in node.children:
+                # For each child, its derivative with respect to the root is
+                # (derivative root -> node) * (derivative node -> child_node)
+                stack.append((child_node, edge_value * child_edge_value))
+                
+        self._partial = dict(partial)
+        
 
     def partial(self,vv):
-        self.backprop()
+        if not self.has_backpropped:
+            self.backprop()
+            self.has_backpropped = True
+        
         if vv in self._partial.keys():
             return self._partial[vv]
+        ##Deal with the case when
+        elif self is vv:
+            return 1
         else:
-            raise KeyError('Function not directly dependent on input')
-            
+            raise KeyError('Function not dependent on input')
+
+    ##Boer Dec 7     
+    def __neg__(self):
+      new=AutoDiffReverse(-self.val)
+      new.name=None
+      new.children=[[self,-1]]
+      return new
+
+    def __inv__(self):
+      new=AutoDiffReverse(1/self.val)
+      new.name=None
+      new.children=[[self,-1/(self.val)**2]]
+      return new
+    
+    def __sub__(self,other):
+      return self+(-other)
+
+    def __rsub__(self,other):
+      return -self+other
+
+    def __truediv__(self,other):
+      try:
+        return self*other.__inv__()
+      except AttributeError:
+        return self*(1/other)
+
+    def __rtruediv__(self,other):
+      return other*self.__inv__()
+
+    def __pow__(self,other):
+      new=AutoDiffReverse(self.val)
+      new.name=None
+      try:
+        new.val**=other.val
+        new.children=[[self,other.val*self.val**(other.val-1)],[other,self.val**other.val*np.log(self.val)]]
+      except:
+        new.val**=other
+        new.children=[[self,other*self.val**(other-1)]]
+      return new
+
+    def __rpow__(self,other):
+      new=AutoDiffReverse(self.val)
+      new.name=None
+      new.val=other**self.val
+      new.children=[[self,other**self.val*np.log(other)]]
+      return new
 
 
-
-
-x=AutoDiffReverse(3)
-y=AutoDiffReverse(4)
-z=AutoDiffReverse(9)
+x=AutoDiffReverse(3, name='x')
+y=AutoDiffReverse(4, name='y')
+z=AutoDiffReverse(9, name='z')
+m=x*y
+print("m",m)
 m=x+y
 n=m*z+x
-print(n.val,n.partial(x))
-print(n.val,n.partial(x))
+
+p=x*x*x
+
+
+
+print(n.val,n.partial(x),n.partial(y),n.partial(z))
+
+print(n._partial)
+assert n.partial(x) == 10
+assert n.partial(y) == 1 * z.val
+assert n.partial(z) == m.val
+
+
+assert m.partial(x) == 1
+assert m.partial(y) == 1
+
+assert p.partial(x)==3*3*3
+
+
+##test_neg
+q=-n
+assert q.partial(x)==-n.partial(x)
+
+##test_sub
+m=x-y
+assert m.val==-1
+assert m.partial(x)==1
+assert m.partial(y)==-1
+m=x-1
+assert m.val==2
+assert m.partial(x)==1
+m=1-x
+assert m.val==-2
+assert m.partial(x)==-1
+
+
+##test_inv
+r=x.__inv__()
+assert  r.partial(x)==-1/x.val**2
+##test_div
+m=x/y
+assert m.val==3/4
+assert m.partial(x)==1/4
+assert m.partial(y)==3*-(4)**(-2)
+m=x/2
+
+assert m.val==1.5
+assert m.partial(x)==1/2
+m=2/x
+assert m.val==2/3
+assert m.partial(x)==2*-(3)**(-2)
+
+##test pow
+m=x**y
+assert m.val==81
+assert m.partial(x)==4*3*3*3
+assert m.partial(y)==3**4*np.log(3)
+
+m=2**x
+assert m.val==8
+assert m.partial(x)==2**3*np.log(2)
