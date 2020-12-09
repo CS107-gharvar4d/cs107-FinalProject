8c1d6af6aff8444e98882d796e137eaa151591d8
Rodrigo Vargas <rov406@g.harvard.edu>
2020-12-08 23:00:09 -0500

    <tests> reverse mode

--- code/tests/test_reverse.py
+++ code/tests/test_reverse.py
@@ -1,2 +1,74 @@
+"""
+ADG4: Automatic Differentiation for Python
 
-#test reverse
+A simple tool for handling automatic differentiation (AD) in Python using
+elementary functions, as well as, trigonometry
+
+Tests module that using AutoDiffReverse and test using backpropagation algo.
+
+"""
+
+import pytest
+import sys
+import ADG4.ad as ad
+import numpy as np
+
+def test_add_mult():
+    """
+    Check basic assertions when adding  or multiplying new values and outputs correct derivative
+    :return:
+    """
+    x = ad.AutoDiffReverse(3, name='x')
+    y = ad.AutoDiffReverse(4, name='y')
+    z = ad.AutoDiffReverse(9, name='z')
+    m = x + y
+    n = m * z + x
+    p = x * x * x
+
+    assert n.partial(x) == 10
+    assert n.partial(y) == 1 * z.val
+    assert n.partial(z) == m.val
+    assert m.partial(x) == 1
+    assert m.partial(y) == 1
+    assert p.partial(x) == 3 * 3 * 3
+
+def test_neg():
+    """
+    Test negative numbers, not inclusive to all positives
+    :return:
+    """
+    x = ad.AutoDiffReverse(3, name='x')
+    y = ad.AutoDiffReverse(4, name='y')
+    z = ad.AutoDiffReverse(-9, name='z')
+    m = x + y
+    n = m * z + x
+    q = -n
+    assert q.partial(x) == -n.partial(x)
+
+def basic_sub():
+    """
+    Test substracion operations upon reverse mode. 
+    :return: 
+    """
+    x = ad.AutoDiffReverse(3, name='x')
+    y = ad.AutoDiffReverse(4, name='y')
+    z = ad.AutoDiffReverse(-9, name='z')
+    m = x - y
+    n = x - y - z
+    assert m.val == -1
+    assert m.partial(x) == 1
+    assert m.partial(y) == -1
+    assert n.val == 8
+    assert n.partial(x) == 1
+    assert n.partial(y) == -1
+    #n.partial(m)
+
+    m = x - 1
+    assert m.val == 2
+    assert m.partial(x) == 1
+    m = 1 - x
+    assert m.val == -2
+    assert m.partial(x) == -1
+    m = z - 1
+    assert m.val == -10
+    #assert  m.partial(y)
