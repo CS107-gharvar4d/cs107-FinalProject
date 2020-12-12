3520285191368372e6c8137833c155036c51ee7d
boerz-coding <62970850+boerz-coding@users.noreply.github.com>
2020-12-11 21:07:40 -0500

    Update setup.py

--- ADG4_Package/setup.py
+++ ADG4_Package/setup.py
@@ -6,10 +6,9 @@ setup(
 	author="Harvard Group 28",
 	description="ADG4 library",
 	packages=find_packages(),
-	install_requires=["numpy", "pytest"],
+	install_requires=["numpy", "pytest","pandas"],
 	long_description="ADG4 library by group 28",
 	long_description_content_type='text/markdown',
 	url="https://github.com/CS107-gharvar4d/cs107-FinalProject",
-	python_requires=">=3.6",
-	install_requires=["numpy","pandas"]
+	python_requires=">=3.6"
 )
