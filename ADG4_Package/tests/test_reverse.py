2444919927d0bcd58b2e1bf6c674c6db19482f4a
boerz-coding <62970850+boerz-coding@users.noreply.github.com>
2020-12-11 21:15:39 -0500

    Update test_reverse.py

--- ADG4_Package/tests/test_reverse.py
+++ ADG4_Package/tests/test_reverse.py
@@ -268,7 +268,7 @@ def test_vconvert():
     vec=rev.AutoDiffReverse.vconvert([m,n,q])
     print(vec.val)
     print(vec.der)
-    result=type('obj',(object,),{'val':[m.val,n.val,q.val],'der':pd.DataFrame([[1,1,0],[-8,-9,7],[8,9,-7]],index=[0,0,0],columns=['x','y','z'])})
+    result=type('obj',(object,),{'val':[m.val,n.val,q.val],'der':pd.DataFrame([[-8,-9,7],[8,9,-7],[1,1,0]],index=[0,0,0],columns=['y','z','x'])})
     print(result.der)
     assert vec.der.equals(result.der)
     
